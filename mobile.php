68a8617ce1f378a3ec487729bf7428244520beeb
krishna gupta <krishnagupta21119@gmail.com>
2024-01-01 00:11:26 +0530

    files added

---
+++ mobile.php
@@ -0,0 +1,305 @@
+<?php
+    session_start();
+    require 'check_if_added.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <div class="container">
+                <div class="jumbotron">
+                    <h1>Welcome to our Store!</h1>
+                    <p>We have the best cameras, watches and Smart Phones for you. No need to hunt around, we have all in one place.</p>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/iphone 12.png" alt="iphone 12">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>IPHONE 12 PRO</h3>
+                                    <p>Price: Rs. 1,00,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(9)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=9" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/iphone se.png" alt="iphone se">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>IPHONE SE</h3>
+                                    <p>Price: Rs. 37,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(10)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=10" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                            
+                        </div>
+                    </div>
+
+                   
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/xiaomi 11.png" alt="xiaomi 11">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>XIAOMI 11</h3>
+                                    <p>Price: Rs. 15,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(10)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=10" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                            
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/iphone 11.png" alt="iphone 11">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>IPHONE 11</h3>
+                                    <p>Price: Rs. 40,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(10)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=10" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                            
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/flip 2.png" alt="flip 2">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>SAMSUNG FLIP 2</h3>
+                                    <p>Price: Rs. 70,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(10)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=10" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                            
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/fold 2.jpg" alt="fold 2">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>SAMSUNG Z-FOLD 2</h3>
+                                    <p>Price: Rs.1,00,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(10)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=10" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                            
+                        </div>
+                    </div>
+
+
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/oppo a58.png" alt="oppo a58">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>OPPO A58</h3>
+                                    <p>Price: Rs. 15,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(11)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=11" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/iphone 12..png" alt="iphone 12">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>IPHONE 12</h3>
+                                    <p>Price: Rs. 50,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(12)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=12" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                            <a href="cart.php">
+                                <img src="img/s20 ultra.png" alt="s20 ultra">
+                            </a>
+                            <center>
+                                <div class="caption">
+                                    <h3>SAMSUNG S20 ULTRA</h3>
+                                    <p>Price: Rs. 90,000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(10)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=10" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                             </div>
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+                <center> 
+                Copyright © 2021 krishna Gupta.
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
